9398f1b16e9835004e088f76c463f8433cd28b0c
Kumar <tusharkumar5@deloitte.com>
2023-12-04 18:20:40 +0530

    First commit

---
+++ src/main/java/controller/AzureSampleApp.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSampleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSampleApp.class, args);
+	}
+}
